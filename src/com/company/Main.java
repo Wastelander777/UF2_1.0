31691756c7af19e102d4b827419fc64e34024838
Pablo Ugarte <ugartep117@gmail.com>
2022-11-11 00:05:59 +0100

    2.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
     public static int calcularAreaTriangulo (int base, int altura){
         // El area de un triangulo es base por altura entre dos
+        // Esta va a ser la version 2.0
         return (base * altura) / 2;
     }
 
     public static void main(String[] args) {
         System.out.println("El area del triangulo es: " + calcularAreaTriangulo(10,15));
+        System.out.println("El area del triangulo es: " + calcularAreaTriangulo(20,45));
     }
 }
